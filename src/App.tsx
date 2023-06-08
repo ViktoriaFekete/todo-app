3433a0f6ecfcce5b38d2042b67b5992c2f90e329
ViktoriaFekete <viktoria.fekete97@gmail.com>
2023-06-08 20:34:53 +0200

    tailwindcss configured

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,16 @@ function App() {
   return (
     <>
       <div>
+          <div className="App">
+              <button className="btn sky-50">Hello daisyUI</button>
+              <button className="h-10 px-6 font-semibold rounded-md bg-black text-white" type="submit">
+                  Buy now</button>
+              <button type="button"
+                      className="bg-indigo-600 text-white text-sm leading-6 font-medium py-2 px-3 rounded-lg">Check
+                  availability
+              </button>
+          </div>
+
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
