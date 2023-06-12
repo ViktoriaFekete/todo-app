4030060d9eba8e87c35350a1d72ec48283640872
ViktoriaFekete <viktoria.fekete97@gmail.com>
2023-06-12 18:21:53 +0200

    basename test

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,7 @@ import { store } from './app/store'
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
         <Provider store={store}>
-            <BrowserRouter basename="/todo-app/">
+            <BrowserRouter basename="/todo-app">
                 <App />
             </BrowserRouter>
         </Provider>
